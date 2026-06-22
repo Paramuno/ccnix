3b3477c8458aeea1f17a1337cef9f869e160c08d
paramuno <wooxgfx@gmail.com>
2026-06-22 19:49:12 +0200

    comment

--- hosts/cc1/hardware-configuration.nix
+++ hosts/cc1/hardware-configuration.nix
@@ -26,6 +26,8 @@
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
+  # Filesystem managed by disko
+
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
