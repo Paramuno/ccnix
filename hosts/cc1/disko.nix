a9d0a51cb77244ef1e895438fc4afccb4b039f91
paramuno <wooxgfx@gmail.com>
2026-06-22 16:07:53 +0200

    disko byidfix

--- hosts/cc1/disko.nix
+++ hosts/cc1/disko.nix
@@ -3,7 +3,7 @@
     disk = {
       cclab-disk = {
         type = "disk";
-        device = "/dev/disk/by-id/a5f1492c-f60d-489c-9902-91e545dd3c38";
+        device = "/dev/disk/by-id/nvme-CT4000T705SSD3_2452E99CE8AD";
         content = {
           type = "gpt";
           partitions = {
