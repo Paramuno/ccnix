98432e2a7f4629b30d4690f8ac87363338c8cda6
paramuno <wooxgfx@gmail.com>
2026-06-22 23:58:08 +0200

    admin root hashed update

--- modules/profiles/admin.nix
+++ modules/profiles/admin.nix
@@ -19,9 +19,9 @@
   users.users.root.openssh.authorizedKeys.keys = [
   ];
 
-  users.users.admin = {
-    hashedPasswordFile = "/etc/admin.pass";
-  };
+  users.users.admin.hashedPasswordFile = "/etc/admin.pass";
+  users.users.root.hashedPasswordFile = "/etc/admin.pass";
+  security.sudo.wheelNeedsPassword = true;
 
   systemd.tmpfiles.rules = [
     "d /home/admin 0700 admin users - -"
