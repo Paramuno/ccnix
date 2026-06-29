f156c3037a3842a07b85c1b705479bdf21448b85
paramuno <wooxgfx@gmail.com>
2026-06-29 19:11:36 +0200

    comfyui cache

--- modules/system/core-config.nix
+++ modules/system/core-config.nix
@@ -37,6 +37,19 @@
     LC_TIME = "de_DE.UTF-8";
   };
 
+  # ── Cache ──────────────────────────────────────────────────────
+
+  nix.settings = {
+    substituters = [
+      "https://comfyui.cachix.org"
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "comfyui.cachix.org-1:33mf9VzoIjzVbp0zwj+fT51HG0y31ZTK3nzYZAX0rec="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   # ── Networking ──────────────────────────────────────────────────────
   networking = {
     networkmanager.enable = true;
