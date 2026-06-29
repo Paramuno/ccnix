53a61249d7215bf7ef71ea561d4c38507cb8a6e8
paramuno <wooxgfx@gmail.com>
2026-06-29 19:44:02 +0200

    customnode1

--- modules/system/comfyui.nix
+++ modules/system/comfyui.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ inputs, pkgs, ... }:
 {
   imports = [ inputs.comfyui-nix.nixosModules.default ];
   nixpkgs.overlays = [ inputs.comfyui-nix.overlays.default ];
@@ -10,5 +10,14 @@
     dataDir = "/var/lib/comfyui";
     listenAddress = "127.0.0.1"; # reach it via SSH tunnel over tailscale
     openFirewall = false;
+
+    customNodes = {
+      ComfyUI-Custom-Scripts = pkgs.fetchFromGitHub {
+        owner = "pythongosssss";
+        repo = "ComfyUI-Custom-Scripts";
+        rev = "609f3afaa74b2f88ef9ce8d939626065e3247469";
+        hash = "sha256-2GgTS7l/sMSnJb07sBifL8NGnDBF3g9qdlSKr3gYFGQ="; # placeholder, fill in below
+      };
+    };
   };
 }
